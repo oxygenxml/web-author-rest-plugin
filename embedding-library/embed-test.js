5a3917a1c63a3643e8df92d7e7bb5bc1210ddebc
coanda.mihai@gmail.com <coanda.mihai@gmail.com>
2016-12-13 14:58:26 +0200

    Improved assembly and development processes

--- embedding-library/embed-test.js
+++ embedding-library/embed-test.js
@@ -1,19 +1,32 @@
 window.onload = function() {
-  var container = document.querySelector('#webapp');
-  var options = {
-    forceTrackChanges: true,
+  document.querySelector('#load').addEventListener('click', load);
+};
 
-    url: 'http://vm17.sync.ro:8081/oxygen-webapp/static/oxygen.html',
-    documentUrl: 'webdav-http://localhost:8081/oxygen-webapp/plugins-dispatcher/webdav-server/dita/flowers/topics/flowers/gardenia.dita'
-  };
+function load() {
+  var container = document.querySelector('#webapp');
+  container.innerHTML = '';
+  var options = getLoadOptions();
+
+  options['url'] = 'http://vm17.sync.ro:8081/oxygen-webapp/static/oxygen.html',
+  options['documentUrl'] = 'webdav-http://localhost:8081/oxygen-webapp/plugins-dispatcher/webdav-server/dita/flowers/topics/flowers/gardenia.dita'
 
   window.editor = new WebAuthor(container, options);
 
+  console.log('Loading with options: ', options);
 
   document.querySelector('#read-only').addEventListener('click', function() {
     window.readonly = window.readonly ? false : true;
     // set READ_ONLY status on the editor.
     window.editor.setReadOnly(window.readonly, 'You do not have editing rights on this document.');
   });
+}
 
-};
+function getLoadOptions() {
+  var options = {};
+  // forceTrackChanges option.
+  var forceTrackChanges = document.querySelector('#forceTrackChanges').checked;
+  if(forceTrackChanges) {
+    options['forceTrackChanges'] = true;
+  }
+  return options;
+}
