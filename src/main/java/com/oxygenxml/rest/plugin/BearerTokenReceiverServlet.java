d2070228a2ce0062eca54a3e09be816c218dca6a
Cristian Talau <cristitalau@gmail.com>
2023-08-03 21:50:03 +0300

    WA-6658: Added servlet that can be used to renew the token.

---
+++ src/main/java/com/oxygenxml/rest/plugin/BearerTokenReceiverServlet.java
@@ -0,0 +1,36 @@
+package com.oxygenxml.rest.plugin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ro.sync.ecss.extensions.api.webapp.plugin.WebappServletPluginExtension;
+import ro.sync.exml.plugin.PluginContext;
+
+public class BearerTokenReceiverServlet extends WebappServletPluginExtension {
+  
+  /**
+   * The headers used for authentication
+   */
+  @PluginContext
+  private AuthHeadersMap authHeadersMap;
+  
+  /**
+   * Saves the bearer token in the headers map.
+   */
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String sessionId = req.getSession().getId();
+    String token = req.getParameter("token");
+    
+    authHeadersMap.setBearerToken(sessionId, token);
+    resp.setStatus(200);
+  }
+
+  @Override
+  public String getPath() {
+    return "rest-bearer-token";
+  }
+}
