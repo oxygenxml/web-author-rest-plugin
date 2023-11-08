c0295b628d50ba085336745ab66fb6fcbf77b9f5
Bina Mircea <bina.mircea2001@gmail.com>
2023-11-08 16:45:49 +0200

    WA-1122: Updated node.js server

--- docs/examples/node_js_api_example/server.js
+++ docs/examples/node_js_api_example/server.js
@@ -1,20 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 3000;
 const BASE_URL = '/api';
 
-app.use(bodyParser.raw({ type: '*/*' })); // To handle octet-stream
-
-// Logging middleware
-app.use((req, res, next) => {
-    const currentTime = new Date().toISOString();
-    console.log(`[${currentTime}] ${req.method} request to ${req.originalUrl}`);
-    next();
-});
-
-// In-memory database
+// In-memory 'database' for files and folders
 const db = {
     files: {
         "document1.xml": "<root>test</root>"
@@ -31,47 +20,75 @@ const db = {
     },
 };
 
-// File operations
+app.use(
+  express.raw({
+      inflate: true,
+      limit: '50mb',
+      type: () => true
+  })
+);
+
+// Logging middleware
+app.use((req, res, next) => {
+    const currentTime = new Date().toISOString();
+    console.log(`[${currentTime}] ${req.method} request to ${req.originalUrl}`);
+    next();
+});
+
+// Helper function to decode and strip prefix from the URL
+function decodeAndStripUrlPrefix(url) {
+    return decodeURIComponent(url || "").replace('rest://cms/', '');
+}
+
+// Route for opening (retrieving) a file
 app.get(`${BASE_URL}/files`, (req, res) => {
-    const decodedFileUrl = decodeURIComponent(req.query.url || "").replace('rest://cms/', '');
+    const decodedFileUrl = decodeAndStripUrlPrefix(req.query.url);
     console.log('Decoded URL:', decodedFileUrl);
+    
     if (db.files[decodedFileUrl]) {
-        return res.send(db.files[decodedFileUrl]);
+        let content = db.files[decodedFileUrl];
+        console.log('file content', content);
+        res.type('application/octet-stream');
+        res.charset = 'utf-8';
+        res.send(content);
+    } else {
+        res.status(404).json({ message: 'File not found' });
     }
-    res.status(404).json({ message: 'File not found' });
 });
 
+// Route for saving (uploading) a file
 app.put(`${BASE_URL}/files`, (req, res) => {
-    const decodedFileUrl = decodeURIComponent(req.query.url || "").replace('rest://cms/', '');
-    console.log('Decoded URL:', decodedFileUrl);
-    db.files[decodedFileUrl] = req.body.toString('utf-8');
-    res.end();
+    const decodedFileUrl = decodeAndStripUrlPrefix(req.query.url);
+    
+    if (Buffer.isBuffer(req.body)) {
+        // Convert buffer to string
+        const content = req.body.toString('utf-8');
+        console.log('Content being stored:', content);
+        
+        // Save or update the file content in the in-memory 'database'
+        db.files[decodedFileUrl] = content;
+        res.status(200).send({ message: "File saved successfully." });
+    } else {
+        // If it's not a buffer, log and return an error
+        console.log('Unexpected req.body type:', req.body);
+        res.status(400).send({ message: "Invalid file data." });
+    }
 });
 
-app.post(`${BASE_URL}/files`, (req, res) => {
-    const decodedFileUrl = decodeURIComponent(req.query.url || "").replace('rest://cms/', '');
-    db.files[decodedFileUrl] = req.body.toString('utf-8');
-    res.end();
-});
 
-app.delete(`${BASE_URL}/files`, (req, res) => {
-    const decodedFileUrl = decodeURIComponent(req.query.url || "").replace('rest://cms/', '');
-    delete db.files[decodedFileUrl];
-    res.end();
-});
-
-// Folder-based browsing widget
+// Route for folder-based browsing
 app.get(`${BASE_URL}/folders`, (req, res) => {
-    const decodedFolderUrl = decodeURIComponent(req.query.url || "").replace('rest://cms/', '');
+    const decodedFolderUrl = decodeAndStripUrlPrefix(req.query.url);
     console.log('Decoded URL:', decodedFolderUrl);
-    console.log('Data:', db.folders[decodedFolderUrl]);
+    
+    // Return the list of files/folders in the requested folder
     res.json(db.folders[decodedFolderUrl] || []);
 });
 
 // Error handler middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({ message: 'Cannot process request due to server error' });
+    res.status(500).json({ message: 'Internal server error' });
 });
 
 app.listen(PORT, () => {
